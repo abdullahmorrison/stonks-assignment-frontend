ce5a3848be49b0aa6a7bd8484083b163e05dc2cf
Abdullah Morrison <abdullahmorrison@gmail.com>
2024-06-26 17:55:52 -0400

    give website info for iframe via .env file

--- src/app/components/Broadcast.tsx
+++ src/app/components/Broadcast.tsx
@@ -2,7 +2,7 @@ export default function Broadcast(){
   return(
     <iframe
       className='flex justify-center items-center w-full h-full'
-      src="https://player.twitch.tv/?channel=alveussanctuary&parent=localhost&muted=true"
+      src={"https://player.twitch.tv/?channel=alveussanctuary&parent="+process.env.WEBSITE+"&muted=true"}
     />
   )
 }
