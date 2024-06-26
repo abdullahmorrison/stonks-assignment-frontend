bb59c5c6ccd9df54b3c5a1b1f0e3d09c822221fd
Abdullah Morrison <abdullahmorrison@gmail.com>
2024-06-26 17:39:50 -0400

    added broadcast and chat components to page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,12 @@
+"use client"
+import Broadcast from "./components/Broadcast"
+import Chat from "./components/Chat"
+
 export default function Stonks() {
   return (
     <main className='flex w-screen h-screen'>
+      <Broadcast/>
+      <Chat/>
     </main>
   )
 }
